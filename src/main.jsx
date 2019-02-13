c5ff216a3b2eb66f50c4a793b8c21bb17e24ce94
Zuray <intheblackworld@gmail.com>
2019-02-13 16:24:04 +0800

    test precommit lint

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { render } from 'react-dom'
 
-const App = () => <div>Hello World</div>
+const App = () => <div>Hello House</div>
 
 render(<App />, document.getElementById('app'))
