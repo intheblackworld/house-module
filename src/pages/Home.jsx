e56065a1c8a7cb6f57c3a790dd56f0ac69480055
Zuray <intheblackworld@gmail.com>
2019-02-15 14:35:13 +0800

    add semantic UI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
+import { Button } from 'semantic-ui-react'
 
-const Home = () => <div>Home</div>
+import 'semantic-ui-css/semantic.min.css'
+
+const Home = () => (
+  <div>
+    Home
+    <Button>Click me</Button>
+  </div>
+)
 
 export default Home
--- webpack.config.js
+++ webpack.config.js
@@ -75,6 +75,22 @@ module.exports = {
           },
         ],
       },
+
+      {
+        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+        loader: require.resolve('url-loader'),
+        options: {
+          limit: 10000,
+          name: 'static/media/[name].[hash:8].[ext]',
+        },
+      },
+      {
+        test: [/\.eot$/, /\.ttf$/, /\.svg$/, /\.woff$/, /\.woff2$/],
+        loader: require.resolve('file-loader'),
+        options: {
+          name: '/static/media/[name].[hash:8].[ext]',
+        },
+      },
     ],
   },
   devServer: {
