3e263cdbc73e8f58a327e35cbfb2e0eabcd70b0b
Zuray <intheblackworld@gmail.com>
2019-02-18 15:43:38 +0800

    modify fullscreen and navigation component style

--- src/components/FullScreen/index.jsx
+++ src/components/FullScreen/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
+import cx from 'classnames'
 import css from './index.scss'
 
 const FullScreen = (props) => {
+  const className = cx(css.fullScreen, {
+    [css.cutHeader]: props.needCutHeader || false,
+  })
   const { children } = props
-  return <div className={css.fullScreen}>{children}</div>
+  return <div className={className}>{children}</div>
 }
 
 FullScreen.defaultProps = {
