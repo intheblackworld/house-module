abde57229c4e8294ff4807485964e91847794d1b
Zuray <intheblackworld@gmail.com>
2019-02-20 19:02:28 +0800

    change build path

--- webpack.config.js
+++ webpack.config.js
@@ -116,7 +116,7 @@ module.exports = {
     ],
   },
   devServer: {
-    contentBase: path.join(__dirname, './dist'),
+    contentBase: path.join(__dirname, './build'),
     compress: true,
     port: 9000,
     historyApiFallback: true,
