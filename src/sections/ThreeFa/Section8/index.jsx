427ab65d8bef2a8407b3ab01e0ae82ba240b205b
Zuray <intheblackworld@gmail.com>
2019-02-27 15:53:29 +0800

    add person image

--- src/sections/ThreeFa/Section8/index.jsx
+++ src/sections/ThreeFa/Section8/index.jsx
@@ -66,49 +66,49 @@ const Slides = ({ show = false }) => {
         initialSlide={initialSlide}
       >
         <div className={css.sliderItem}>
-          <Image src={`https://via.placeholder.com/${200}x${300}`} alt="" />
+          <Image src={require('./person1.png')} alt="" />
           <div className={css.sliderContent}>
             <p>建築大師 國泰御用 豪宅器度 精鑄帝標</p>
             <h3>建築師 葛介南</h3>
           </div>
         </div>
         <div className={css.sliderItem}>
-          <Image src={`https://via.placeholder.com/${200}x${300}`} alt="" />
+          <Image src={require('./person2.png')} alt="" />
           <div className={css.sliderContent}>
             <p>結構權威 耐震行家 國泰指定 冠德信賴</p>
             <h3>結構技師 劉國欽</h3>
           </div>
         </div>
         <div className={css.sliderItem}>
-          <Image src={`https://via.placeholder.com/${200}x${300}`} alt="" />
+          <Image src={require('./person3.png')} alt="" />
           <div className={css.sliderContent}>
             <p>工程精英 豪宅實力 優秀品質</p>
             <h3>公務協理 馮志華</h3>
           </div>
         </div>
         <div className={css.sliderItem}>
-          <Image src={`https://via.placeholder.com/${200}x${300}`} alt="" />
+          <Image src={require('./person4.png')} alt="" />
           <div className={css.sliderContent}>
             <p>英雄盛會 獨領風騷 雄霸江翠</p>
             <h3>董事長 賴運興</h3>
           </div>
         </div>
         <div className={css.sliderItem}>
-          <Image src={`https://via.placeholder.com/${200}x${300}`} alt="" />
+          <Image src={require('./person5.png')} alt="" />
           <div className={css.sliderContent}>
             <p>工程精英 豪宅實力 優秀品質</p>
             <h3>公務經理 劉春平</h3>
           </div>
         </div>
         <div className={css.sliderItem}>
-          <Image src={`https://via.placeholder.com/${200}x${300}`} alt="" />
+          <Image src={require('./person6.png')} alt="" />
           <div className={css.sliderContent}>
             <p>公設名家 華人之光 金獎名家 橫掃亞太</p>
             <h3>主持設計師 許家福</h3>
           </div>
         </div>
         <div className={css.sliderItem}>
-          <Image src={`https://via.placeholder.com/${200}x${300}`} alt="" />
+          <Image src={require('./person7.png')} alt="" />
           <div className={css.sliderContent}>
             <p>公設名家 華人之光 金獎名家 橫掃亞太</p>
             <h3>主持設計師 林之豐</h3>
