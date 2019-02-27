0ddd090eed7cd913496e749b08e32848bc95c8e1
Zuray <intheblackworld@gmail.com>
2019-02-27 18:09:13 +0800

    modify style

--- src/sections/ThreeFa/Section10/index.jsx
+++ src/sections/ThreeFa/Section10/index.jsx
@@ -2,17 +2,16 @@ import React from 'react'
 import SectionTitle from 'components/SectionTitle'
 import CircleAnimate from 'components/CircleAnimate'
 import ViewPort from 'components/ViewPort'
-// import cx from 'classnames'
 
-import './index.scss'
+import css from './index.scss'
 
 const Section10 = () => (
-  <React.Fragment>
+  <div className={css.bg}>
     <SectionTitle title="五大友善設備 生活加倍舒適" />
     <ViewPort>
       <CircleAnimate />
     </ViewPort>
-  </React.Fragment>
+  </div>
 )
 
 export default Section10
