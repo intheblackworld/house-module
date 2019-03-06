a6c27fa8f87e0ade2444b5c1db878ce5dc0646d6
Zuray <intheblackworld@gmail.com>
2019-03-06 23:33:39 +0800

    modify style

--- src/projects/star/MobileSection/index.jsx
+++ src/projects/star/MobileSection/index.jsx
@@ -1,18 +1,10 @@
-import { useState } from 'react'
 import { Desc } from 'projects/star/Section4'
 import c from './index.scss'
 
-const MobileSection = () => {
-  const [show, setShow] = useState(false)
-
-  return (
-    <div className={c.bg}>
-      {!show && (
-        <div className={c.export} onKeyDown={() => setShow(!show)} onClick={() => setShow(!show)} />
-      )}
-      {show && <Desc />}
-    </div>
-  )
-}
+const MobileSection = () => (
+  <div className={c.bg}>
+    <Desc />
+  </div>
+)
 
 export default MobileSection
