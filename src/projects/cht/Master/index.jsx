41099f433bbfa6490f269f783571173a51a82af2
Zuray David <intheblackworld@gmail.com>
2019-04-13 16:14:53 +0800

    modify some style

--- src/projects/cht/Master/index.jsx
+++ src/projects/cht/Master/index.jsx
@@ -5,12 +5,15 @@ import c from './index.scss'
 
 const Master = ({ show }) => {
   const bgClass = withTrans('bg', c, show)
+  const colorBgClass = withTrans('colorBg', c, show)
 
   return (
     <div className={`${bgClass} masterBg`}>
-      <div className={c.container}>
-        <img src={require('./title.png')} alt="長虹天際的圖片" className={c.title} />
-        <p className={`${c.subTitle} flex-c`}>公園富人區 地標落成{ !isMobile && '˙' }  70-83坪</p>
+      <div className={`${colorBgClass} masterBg`}>
+        <div className={c.container}>
+          <img src={require('./title.png')} alt="長虹天際的圖片" className={c.title} />
+          <p className={`${c.subTitle} flex-c`}>公園富人區 地標落成{ !isMobile && '˙' }  70-83坪</p>
+        </div>
       </div>
     </div>
   )
