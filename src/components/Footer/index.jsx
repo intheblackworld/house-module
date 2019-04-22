5968cb8a07d0a798e89b88a58c88ab0fa5612fd4
jingting <jingting0722@gmail.com>
2019-04-22 19:35:00 +0800

    jt push test

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 import css from './index.scss'
 
 const Footer = ({ name = 'LIXIN DIGITAL' }) => (
-  
   <div className={css.footer}>
     <p>{name}</p>
     <a href=" https://www.h35.tw/admin/test/login.php" target="_blank">網頁製作</a>
