e26e2cf54db69a0b90dc16e557f03a2ef3ceab37
jingting <jingting0722@gmail.com>
2019-04-30 18:39:11 +0800

    嘗試解決客戶反應問題：點擊輸入框或選項框，鍵盤跳出後表單不見，畫面往上跳或表單被切掉

--- src/layouts/ContactSection2/index.jsx
+++ src/layouts/ContactSection2/index.jsx
@@ -5,7 +5,7 @@
 import Viewport from 'components/ViewPort'
 import GhostOrder from 'components/GhostOrder'
 import FooterContactInfo from 'components/FooterContactInfo'
-import FullScreen from 'layouts/FullScreen'
+// import FullScreen from 'layouts/FullScreen'
 // import Section from 'layouts/Section'
 import GoogleMap from 'components/GoogleMap'
 // import HouseInfo from 'components/HouseInfo'
@@ -18,10 +18,8 @@ const {
 const ContactSection = () => (
   <div className={c.contactSection}>
     <GoogleMap />
-    <div className={`${c.orderBg} bg`}>
-      <FullScreen needCutHeader alignCenter>
-        <GhostOrder />
-      </FullScreen>
+    <div id="orderBg" className={`${c.orderBg} bg`}>
+      <GhostOrder />
     </div>
     <Viewport isBottom={false}>
       <FooterContactInfo
