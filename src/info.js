6d8d981d7bd8abe5ab85c0c1bcca4a59de6e5873
Zuray David <intheblackworld@gmail.com>
2019-05-23 18:29:43 +0800

    change phone number

--- src/info.js
+++ src/info.js
@@ -1,7 +1,7 @@
 export default {
   address: '244新北市林口區文化三路二段292號',
   locations: { lat: 25.0832356, lng: 121.3769826 },
-  phone: '02-2268-8765',
+  phone: '02-2262-3888',
   fbLink: 'https://www.facebook.com/%E9%95%B7%E8%99%B9%E5%A4%A9%E9%9A%9B-2731739126866550/',
   googleLink: 'https://goo.gl/maps/7cv2w5gGjgp',
   caseName: '長虹天際',
