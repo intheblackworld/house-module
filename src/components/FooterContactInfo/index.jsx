52d3fb1f0496a7c3afae84a86c11a36bb53d1c51
Zuray David <intheblackworld@gmail.com>
2019-05-29 21:59:46 +0800

    add precheck dialog and modify style to infoBlock

--- src/components/FooterContactInfo/index.jsx
+++ src/components/FooterContactInfo/index.jsx
@@ -1,26 +1,143 @@
 /* eslint-disable jsx-a11y/iframe-has-title */
 // import { withTrans } from 'utils'
 // import ViewPort from 'components/ViewPort'
+import { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import { CallDialog, MapDialog } from 'components/Dialog'
+import cx from 'classnames'
 import { isMobile } from '../../utils'
 import c from './index.scss'
 
+// const InfoBlock = ({ show }) => {
+//   const [isCallShow, toggleCallDialog] = useState(false)
+
+//   const showCallDialog = () => {
+//     toggleCallDialog(!isCallShow)
+//   }
+
+//   const closeCallDialog = () => {
+//     toggleCallDialog(false)
+//   }
+
+//   const [isMapShow, toggleMapDialog] = useState(false)
+
+//   const showMapDialog = () => {
+//     toggleMapDialog(!isMapShow)
+//   }
+
+//   const closeMapDialog = () => {
+//     toggleMapDialog(false)
+//   }
+
+//   const closeCallClass = cx(c.closeCall, {
+//     [c.show]: isCallShow,
+//   })
+
+//   const closeMapClass = cx(c.closeMap, {
+//     [c.show]: isMapShow,
+//   })
+//   return (
+//     <div className={withTrans('infoBg', c, show)}>
+//       <CallDialog show={isCallShow} closeDialog={closeCallDialog} />
+//       <div className={closeCallClass} onClick={closeCallDialog} onKeyDown={closeCallDialog}>
+//         <FontAwesomeIcon icon={faTimes} />
+//       </div>
+//       <MapDialog show={isMapShow} closeDialog={closeMapDialog} />
+//       <div className={closeMapClass} onClick={closeMapDialog} onKeyDown={closeMapDialog}>
+//         <FontAwesomeIcon icon={faTimes} />
+//       </div>
+//       <div className={c.infoContainer}>
+//         <img src={require('./infoLogo.png')} alt="" />
+//         <div className={c.infoList}>
+//           <div>
+//             {isMobile ? (
+//               // eslint-disable-next-line jsx-a11y/click-events-have-key-events
+//               <a target="_blank" rel="noopener noreferrer" onClick={showCallDialog}>
+//                 <FontAwesomeIcon icon={faPhone} />
+//                 {info.phone}
+//               </a>
+//             ) : (
+//               <p>
+//                 <FontAwesomeIcon icon={faPhone} />
+//                 {info.phone}
+//               </p>
+//             )}
+//           </div>
+//           <div>
+//             <a href={info.fbMessage} target="_blank" rel="noopener noreferrer">
+//               FB Messenger諮詢
+//             </a>
+//           </div>
+
+//           <div>
+//             {isMobile ? (
+//               /* eslint-disable-next-line jsx-a11y/click-events-have-key-events */
+//               <a onClick={showMapDialog} rel="noopener noreferrer">
+//                 導航google地圖
+//                 <br />
+//                 <span style={{ fontSize: '14px' }}>{info.address}</span>
+//               </a>
+//             ) : (
+//               <a href={info.googleLink} target="_blank" rel="noopener noreferrer">
+//                 導航google地圖
+//                 <br />
+//                 <span style={{ fontSize: '14px' }}>{info.address}</span>
+//               </a>
+//             )}
+//           </div>
+
+//           <div>
+//             <a href={info.fbLink} target="_blank" rel="noopener noreferrer">
+//               前往粉絲專頁
+//             </a>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
 const FooterContactInfo = ({
-  address, phone, googleLink, houseInfos,
+  address, phone, houseInfos, googleLink,
 }) => {
-  const redirectToPhoneThanks = (e) => {
-    e.preventDefault()
-    if (isMobile) {
-      window.location.href = `tel:${phone.replace('-', '')}`
-    } else {
-      return
-    }
-    setTimeout(() => {
-      window.location.href = 'phoneThanks'
-    }, 1000)
+  const [isCallShow, toggleCallDialog] = useState(false)
+
+  const showCallDialog = () => {
+    toggleCallDialog(!isCallShow)
   }
+
+  const closeCallDialog = () => {
+    toggleCallDialog(false)
+  }
+
+  const [isMapShow, toggleMapDialog] = useState(false)
+
+  const showMapDialog = () => {
+    toggleMapDialog(!isMapShow)
+  }
+
+  const closeMapDialog = () => {
+    toggleMapDialog(false)
+  }
+
+  const closeCallClass = cx(c.closeCall, {
+    [c.show]: isCallShow,
+  })
+
+  const closeMapClass = cx(c.closeMap, {
+    [c.show]: isMapShow,
+  })
 
   return (
     <div className={c.bg}>
+      <CallDialog show={isCallShow} closeDialog={closeCallDialog} />
+      <div className={closeCallClass} onClick={closeCallDialog} onKeyDown={closeCallDialog}>
+        <FontAwesomeIcon icon={faTimes} />
+      </div>
+      <MapDialog show={isMapShow} closeDialog={closeMapDialog} />
+      <div className={closeMapClass} onClick={closeMapDialog} onKeyDown={closeMapDialog}>
+        <FontAwesomeIcon icon={faTimes} />
+      </div>
       <div className={c.container}>
         <img src={require('./logo.png')} alt="" className={c.logo} />
         <div className={c.content}>
@@ -36,16 +153,22 @@ const FooterContactInfo = ({
           </div>
           <div className={c.contactInfo}>
             <h3>聯絡資訊</h3>
-            <a
-              href={`tel:${phone.replace('-', '')}`}
-              rel="noopener noreferrer"
-              onClick={redirectToPhoneThanks}
-            >
-              賞屋專線：{phone}
-            </a>
-            <a href={googleLink} target="_blank" rel="noopener noreferrer">
-              接待會館：{address}
-            </a>
+            {isMobile ? (
+              // eslint-disable-next-line
+              <p onClick={showCallDialog}>賞屋專線：{phone}</p>
+            ) : (
+              <p>賞屋專線：{phone}</p>
+            )}
+
+            {isMobile ? (
+              // eslint-disable-next-line
+              <p onClick={showMapDialog}>接待會館：{address}</p>
+            ) : (
+              <a href={googleLink} target="_blank" rel="noopener noreferrer">
+                接待會館：{address}
+              </a>
+            )}
+
             {/* <p>賞屋專線：{phone}</p>
             <p>接待會館：{address}</p> */}
             <div className={c.fbBlock}>
