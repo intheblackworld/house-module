90363327878f336e7929a755d828c0ef0c3215e3
Zuray David <intheblackworld@gmail.com>
2019-06-13 16:38:14 +0700

    調整樣式

--- src/projects/zy/Master/index.jsx
+++ src/projects/zy/Master/index.jsx
@@ -1,14 +1,16 @@
 import { withTrans } from 'utils'
 // import ViewPort from 'components/ViewPort'
 // import ImageBg from 'components/ImageBg'
-// import { isMobile } from '../../../utils'
+import { isMobile } from '../../../utils'
 import c from './index.scss'
 
 const Master = ({ show }) => {
   const bgClass = withTrans('bg', c, show)
   // const colorBgClass = withTrans('colorBg', c, show)
 
-  return <div className={`${bgClass} masterBg`} />
+  return (
+    <img className={bgClass} src={isMobile ? require('./m1.jpg') : require('./d1.jpg')} alt="" />
+  )
 }
 
 export default Master
