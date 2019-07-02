dd5b70adb7933c826575c5e47a9f3a0dd5715b4b
Zuray David <intheblackworld@gmail.com>
2019-07-02 14:27:22 +0800

    replace google map API to iframe

--- src/info.js
+++ src/info.js
@@ -1,6 +1,7 @@
 export default {
   address: '221新北市汐止區康寧街457號',
-  locations: { lat: 25.068579, lng: 121.63142 },
+  googleSrc:
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3613.9827148898416!2d121.62923131549852!3d25.068574983956196!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x345d534c7e98316d%3A0xa614b98d1cda13f8!2zMjIx5paw5YyX5biC5rGQ5q2i5Y2A5bq35a-n6KGXNDU36Jmf!5e0!3m2!1szh-TW!2stw!4v1562048804690!5m2!1szh-TW!2stw',
   phone: '02-2692-1155',
   fbLink:
     'https://www.facebook.com/%E6%B1%90%E6%AD%A2%E6%9D%B1%E5%9F%8E%E5%A4%A7%E5%A2%83-584895791902170/',
