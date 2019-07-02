4d1bc9dd0e571b14ebe3c88db357f957c1a352a1
Zuray David <intheblackworld@gmail.com>
2019-07-02 14:22:45 +0800

    replace google map API to iframe

--- src/info.js
+++ src/info.js
@@ -1,6 +1,6 @@
 export default {
   address: '244新北市林口區文化三路二段292號',
-  locations: { lat: 25.0832356, lng: 121.3769826 },
+  googleSrc: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3613.550039486564!2d121.37479391549893!3d25.083235583949325!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a6c283d9ba55%3A0xfb3786a66bb691fc!2zMjQ05paw5YyX5biC5p6X5Y-j5Y2A5paH5YyW5LiJ6Lev5LqM5q61Mjky6Jmf!5e0!3m2!1szh-TW!2stw!4v1562048494412!5m2!1szh-TW!2stw',
   phone: '02-2622-3888',
   fbLink: 'https://www.facebook.com/%E9%95%B7%E8%99%B9%E5%A4%A9%E9%9A%9B-2731739126866550/',
   fbMessage: 'https://m.me/2731739126866550',
