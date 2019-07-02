e62cccd53bfd583ff2dd7d2dd781494c8c895c99
Zuray David <intheblackworld@gmail.com>
2019-07-02 14:42:55 +0800

    replace google map API to iframe

--- src/components/GoogleMap/index.jsx
+++ src/components/GoogleMap/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import sliverStyle from './sliver'
+// import sliverStyle from './sliver'
 import info from '../../info'
-import css from './index.scss'
+// import css from './index.scss'
 
-const loadJS = (src) => {
-  const ref = window.document.getElementsByTagName('script')[0]
-  const script = window.document.createElement('script')
-  script.src = src
-  script.async = true
-  ref.parentNode.insertBefore(script, ref)
-}
+// const loadJS = (src) => {
+//   const ref = window.document.getElementsByTagName('script')[0]
+//   const script = window.document.createElement('script')
+//   script.src = src
+//   script.async = true
+//   ref.parentNode.insertBefore(script, ref)
+// }
 
 export default class GooglMap extends React.Component {
   constructor(props) {
@@ -17,37 +17,45 @@ export default class GooglMap extends React.Component {
     this.mapArea = React.createRef()
   }
 
-  componentDidMount() {
-    window.initMap = this.initMap
-    this.styledMapType = loadJS(
-      'https://maps.googleapis.com/maps/api/js?key=AIzaSyAKNXcM_2Jf2ZLNbFBe4blWitnvKXM1CSo&callback=initMap',
-    )
-  }
+  // componentDidMount() {
+  //   window.initMap = this.initMap
+  //   this.styledMapType = loadJS(
+  //     'https://maps.googleapis.com/maps/api/js?key=AIzaSyAKNXcM_2Jf2ZLNbFBe4blWitnvKXM1CSo&callback=initMap',
+  //   )
+  // }
 
-  initMap = () => {
-    const { google } = window
-    // 座標位置
-    const position = { lat: info.locations.lat, lng: info.locations.lng }
-    const map = new google.maps.Map(this.mapArea.current, {
-      center: position,
-      zoom: 18,
-      mapTypeControlOptions: {
-        mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain', 'styled_map'],
-      },
-    })
-    map.mapTypes.set('styled_map', new google.maps.StyledMapType(sliverStyle))
-    map.setMapTypeId('styled_map')
-    const marker = new google.maps.Marker({ // eslint-disable-line
-      position,
-      map,
-    })
-  }
+  // initMap = () => {
+  //   const { google } = window
+  //   // 座標位置
+  //   const position = { lat: info.locations.lat, lng: info.locations.lng }
+  //   const map = new google.maps.Map(this.mapArea.current, {
+  //     center: position,
+  //     zoom: 18,
+  //     mapTypeControlOptions: {
+  //       mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain', 'styled_map'],
+  //     },
+  //   })
+  //   map.mapTypes.set('styled_map', new google.maps.StyledMapType(sliverStyle))
+  //   map.setMapTypeId('styled_map')
+  //   const marker = new google.maps.Marker({
+  //     // eslint-disable-line
+  //     position,
+  //     map,
+  //   })
+  // }
 
   render() {
     return (
-      <div style={{ position: 'relative', background: '#fff' }}>
-        <img src={require('./map.png')} alt="" className={css.mapImage} />
-        <div ref={this.mapArea} className={css.mapFrame} />
+      <div style={{ display: 'flex' }}>
+        <iframe
+          title="googlemap"
+          src={info.googleSrc}
+          width="100%"
+          height="455"
+          frameBorder="0"
+          style={{ border: 0 }}
+          allowFullScreen
+        />
       </div>
     )
   }
--- src/info.js
+++ src/info.js
@@ -1,9 +1,9 @@
 export default {
   address: '台北市中山區建國北路二段76號',
-  locations: { lat: 25.055106, lng: 121.536435 },
+  googleSrc:
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3614.3803032886954!2d121.5342462154983!3d25.05509618396247!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a95ff19e9fcb%3A0xc8f4da3f415018fb!2zMTA0OTHlj7DljJfluILkuK3lsbHljYDlu7rlnIvljJfot6_kuozmrrU3NuiZnw!5e0!3m2!1szh-TW!2stw!4v1562049729036!5m2!1szh-TW!2stw',
   phone: '02-2516-5888',
-  fbLink:
-    'https://www.facebook.com/greatdici/',
+  fbLink: 'https://www.facebook.com/greatdici/',
   fbMessage: 'https://m.me/greatdici/',
   googleLink: 'https://goo.gl/maps/mAGcEpHjfzJCtPS68',
   caseName: '帝璽',
