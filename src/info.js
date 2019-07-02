af4f1d8c38367d9a21c3e432340eccd6956bb525
Zuray David <intheblackworld@gmail.com>
2019-07-02 14:51:02 +0800

    replace google map API to iframe

--- src/info.js
+++ src/info.js
@@ -1,6 +1,7 @@
 export default {
   address: '23943 新北市鶯歌區永和街83號',
   phone: '02-26771133',
+  googleSrc: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3616.9431643434764!2d121.33086051534507!3d24.96804828400333!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34681eb8c153e3cd%3A0xa8ce824e9675e026!2zMjM55paw5YyX5biC6bav5q2M5Y2A5rC45ZKM6KGXODPomZ8!5e0!3m2!1szh-TW!2stw!4v1562050204274!5m2!1szh-TW!2stw',
   fbLink: 'https://www.facebook.com/sanfarlightluxury',
   fbMessage: 'https://m.me/sanfarlightluxury',
   googleLink: 'https://goo.gl/maps/3YwsMdrBB582',
