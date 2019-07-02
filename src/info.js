b418f787aeac5063ceab05c1e4654e88f58a3831
Zuray David <intheblackworld@gmail.com>
2019-07-02 15:03:58 +0800

    replace google map API to iframe

--- src/info.js
+++ src/info.js
@@ -1,6 +1,7 @@
 export default {
   address: '108台北市萬華區長沙街二段148號',
-  locations: { lat: 25.0405227, lng: 121.5013042 },
+  googleSrc:
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3614.8065766829764!2d121.49913401534641!3d25.0406373839692!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a9a818741829%3A0xa6aed82dce1c51ef!2zMTA45Y-w5YyX5biC6JCs6I-v5Y2A6ZW35rKZ6KGX5LqM5q61MTQ46Jmf!5e0!3m2!1szh-TW!2stw!4v1562050982351!5m2!1szh-TW!2stw',
   phone: '02-2370-8588',
   fbLink: 'https://www.facebook.com/GreatManor/?modal=admin_todo_tour',
   fbMessage: 'https://m.me/GreatManor',
