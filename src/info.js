cca12ed8a29bcb501f49f502f77fb2e038706370
Zuray David <intheblackworld@gmail.com>
2019-07-02 14:46:16 +0800

    replace google map API to iframe

--- src/info.js
+++ src/info.js
@@ -1,6 +1,8 @@
 export default {
   address: '土城區中央路四段26號',
   phone: '02-2268-8765',
+  googleSrc:
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3617.147414420562!2d121.41869561534497!3d24.961098684006668!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34681ce9ba493fe3%3A0x616e970e4d620b73!2zMjM25paw5YyX5biC5Zyf5Z-O5Y2A5Lit5aSu6Lev5Zub5q61MjbomZ8!5e0!3m2!1szh-TW!2stw!4v1562049924115!5m2!1szh-TW!2stw',
   fbLink: 'https://www.facebook.com/%E5%BE%B7%E5%8F%8B%E8%97%8F-263145487638369/',
   fbMessage: 'https://m.me/263145487638369',
   googleLink: 'https://goo.gl/maps/3gCXyCLZwhB2',
