fd4f7dc6b3785c2fd1d61d888064d95051707d5e
Zuray David <intheblackworld@gmail.com>
2019-07-04 18:15:28 +0800

    modify address

--- src/info.js
+++ src/info.js
@@ -1,12 +1,12 @@
 export default {
-  address: '新北市五股區新五路三段二號旁',
+  address: '新北市五股區新五路二段558號',
   googleSrc:
-    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d903.3604047919677!2d121.44342382921391!3d25.08690799899673!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjXCsDA1JzEyLjkiTiAxMjHCsDI2JzM4LjMiRQ!5e0!3m2!1szh-TW!2stw!4v1562051444263!5m2!1szh-TW!2stw',
+    'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3613.4864844017407!2d121.4406097154987!3d25.085388383948263!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442a61c578c9875%3A0xbb77c37b8b751fc5!2zMjQ45paw5YyX5biC5LqU6IKh5Y2A5paw5LqU6Lev5LqM5q61NTU46Jmf!5e0!3m2!1szh-TW!2stw!4v1562235294213!5m2!1szh-TW!2stw',
   phone: '02-22931666',
   fbLink: 'https://www.facebook.com/正義聯盟-小宅變身-正義看我-2066330633489731/',
   fbMessage: 'https://m.me/2066330633489731',
   googleLink: 'https://goo.gl/maps/ikUZ7Uvuy7Koco2Q7',
   caseName: '正義聯盟',
 
-  houseInfos: [['接待中心', '新北市五股區新五路三段二號旁'], ['聯絡電話', '02-22931666']],
+  houseInfos: [['接待中心', '新北市五股區新五路二段558號'], ['聯絡電話', '02-22931666']],
 }
