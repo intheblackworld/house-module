b6c01f4c81e1f57c3d8cdc6835cff82c3ddf8bfa
Zuray David <intheblackworld@gmail.com>
2019-11-04 17:14:18 +0800

    modify

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import Navigation from 'components/Navigation'
+import ActivityDialog from 'components/ActivityDialog'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import cx from 'classnames'
 import MobileNavigation from 'components/MobileNavigation'
 import Layout from 'layouts'
 import Footer from 'components/Footer'
+import c from './index.scss'
 import { isMobile } from '../utils'
 
 // const Home = () => (
@@ -20,6 +25,9 @@ export default class Home extends React.Component {
     super(props)
     this.script = React.createRef()
     this.noscript = React.createRef()
+    this.state = {
+      isShow: false,
+    }
   }
 
   componentDidMount() {
@@ -39,13 +47,25 @@ export default class Home extends React.Component {
     height="0" width="0" style="display:none;visibility:hidden"></iframe>`
     this.script.current.appendChild(s)
     this.noscript.current.appendChild(n)
+    this.setState({ isShow: true })
+  }
+
+  closeDialog = () => {
+    this.setState({ isShow: false })
   }
 
   render() {
+    const closeClass = cx(c.close, {
+      [c.show]: this.state.isShow,
+    })
     return (
       <div>
         <div ref={this.script} />
         <div ref={this.noscript} />
+        <ActivityDialog show={this.state.isShow} />
+        <div className={closeClass} onClick={this.closeDialog} onKeyDown={this.closeDialog}>
+          <FontAwesomeIcon icon={faTimes} />
+        </div>
         <Navigation />
         { isMobile && <MobileNavigation />}
         <Layout />
