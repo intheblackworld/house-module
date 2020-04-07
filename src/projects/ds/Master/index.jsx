e7fbc8968c7331464c645b98544327d8db90012b
Zuray David <intheblackworld@gmail.com>
2020-04-07 17:02:50 +0800

    modify

--- src/projects/ds/Master/index.jsx
+++ src/projects/ds/Master/index.jsx
@@ -1,21 +1,54 @@
 import { withTrans } from 'utils'
 // import ViewPort from 'components/ViewPort'
 // import ImageBg from 'components/ImageBg'
-import { isMobile } from '../../../utils'
+// import { isMobile } from '../../../utils'
 import c from './index.scss'
 
 const Master = ({ show }) => {
   const containerClass = withTrans('container', c, show)
-  const bgClass = withTrans('bg', c, show)
+  // const bgClass = withTrans('bg', c, show)
   // const colorBgClass = withTrans('colorBg', c, show)
 
   return (
-    <div className={containerClass}>
-      <img className={bgClass} src={isMobile ? require('./m1.png') : require('./d1.png')} alt="帝璽的圖片" />
-      <img src={require('./logo-1.png')} alt="" className={c.logo1} />
-      <img src={require('./logo-2.png')} alt="" className={c.logo2} />
-      <img src={require('./title-1.png')} alt="" className={c.title1} />
-      <img src={require('./title-2.png')} alt="" className={c.title2} />
+    <div className={`${containerClass} ${c.section1}`}>
+      <div className={c.img1}>
+        <img src={require('./bg1.jpg')} alt="" /><img src={require('./bg1.jpg')} alt="" />
+      </div>
+      <div className={c.img2}>
+        <img src={require('./cloud.png')} alt="" /><img src={require('./cloud.png')} alt="" /><img
+          src={require('./cloud.png')}
+          alt=""
+        />
+      </div>
+      <div className={c.img31}>
+        <picture>
+          <source srcSet={require('./pc_00.png')} media="(min-width: 750px)" />
+          <source srcSet={require('./mo/pc_00.png')} media="(max-width: 750px)" />
+          <img src={require('./pc_00.png')} alt="" />
+        </picture>
+      </div>
+      <div className={c.img3}>
+        <picture>
+          <source srcSet={require('./pc_01.png')} media="(min-width: 750px)" />
+          <source srcSet={require('./mo/pc_01.png')} media="(max-width: 750px)" />
+          <img src={require('./pc_01.png')} alt="" />
+        </picture>
+      </div>
+      <div className={c.img31}>
+        <picture>
+          <source srcSet={require('./pc_02.png')} media="(min-width: 750px)" />
+          <source srcSet={require('./mo/pc_02.png')} media="(max-width: 750px)" />
+          <img src={require('./pc_02.png')} alt="" />
+        </picture>
+      </div>
+      <div className={c.img4}>
+        <picture>
+          <source srcSet={require('./logo.png')} media="(min-width: 750px)" />
+          <source srcSet={require('./mo/logo.png')} media="(max-width: 750px)" />
+          <img src={require('./logo.png')} alt="" />
+        </picture>
+      </div>
+      <div className={c.img5}><img src={require('./bg1.png')} alt="" /></div>
     </div>
   )
 }
