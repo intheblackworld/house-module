487108528ecb060da98c7cc864384d7491917ba2
Zuray David <intheblackworld@gmail.com>
2020-07-30 18:16:20 +0800

    modify

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -39,6 +39,23 @@ export default class Home extends React.Component {
     height="0" width="0" style="display:none;visibility:hidden"></iframe>`
     this.script.current.appendChild(s)
     this.noscript.current.appendChild(n)
+
+    const s2 = document.createElement('script')
+    s.type = 'text/javascript'
+    s.async = true
+    s.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-5RVX2N');`
+
+    const n2 = document.createElement('noscript')
+    n.type = 'text/javascript'
+    n.async = true
+    n.innerHTML = `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5RVX2N"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+    this.script.current.appendChild(s2)
+    this.noscript.current.appendChild(n2)
   }
 
   render() {
