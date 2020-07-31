ea81ca379e63ff2373e94e11849496e9d976db49
Zuray David <intheblackworld@gmail.com>
2020-07-31 12:39:20 +0800

    modify

--- src/pages/FormThanks.jsx
+++ src/pages/FormThanks.jsx
@@ -17,32 +17,15 @@ export default class Thanks extends React.Component {
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-WK8TNQN');`
+    })(window,document,'script','dataLayer','GTM-5RVX2N5');`
 
     const n = document.createElement('noscript')
     n.type = 'text/javascript'
     n.async = true
-    n.innerHTML = `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WK8TNQN"
+    n.innerHTML = `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5RVX2N5"
     height="0" width="0" style="display:none;visibility:hidden"></iframe>`
     this.script.current.appendChild(s)
     this.noscript.current.appendChild(n)
-
-    const s2 = document.createElement('script')
-    s.type = 'text/javascript'
-    s.async = true
-    s.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-5RVX2N5');`
-
-    const n2 = document.createElement('noscript')
-    n.type = 'text/javascript'
-    n.async = true
-    n.innerHTML = `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5RVX2N5"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe>`
-    this.script.current.appendChild(s2)
-    this.noscript.current.appendChild(n2)
 
 
     const line = document.createElement('script')
